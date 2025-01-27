99e02bf58d96cd6390d632a7f65ae8d1751266ab
Marius Constantin <68522001+realmariusconstantin@users.noreply.github.com>
2025-01-27 16:14:41 +0000

    README update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <main>
+    <admin-page></admin-page>
+    </main>
+
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import {defineComponent} from "vue";
+import AdminPage from "@/pages/AdminPage.vue";
+
+export default defineComponent({
+  components: {AdminPage}
+})
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh; /* Make sure it only fills the viewport */
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  flex-grow: 1; /* Allows main content to expand while keeping footer at the bottom */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+footer {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+  background-color: #000000;
+  color: #ffffff;
 }
 </style>
